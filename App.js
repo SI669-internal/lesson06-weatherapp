6a776e0839635ca46f3d5265c2b48aca8c86b203
Mark Newman <mwnewman@umich.edu>
2020-10-01 00:28:40 -0400

    added custom component

--- App.js
+++ App.js
@@ -82,7 +82,7 @@ export class Step2 extends React.Component {
   }
 }
 
-export default class Step3 extends React.Component {
+export class Step3 extends React.Component {
 
   constructor() {
     super();
@@ -123,6 +123,78 @@ export default class Step3 extends React.Component {
                 <Text style={styles.cwItemText}>{item.name}</Text>
                 <Text style={styles.cwItemText}>{item.temp}</Text>
               </View>
+            }
+            />
+        </View>
+        <View style={styles.footer}>
+          <TouchableOpacity 
+            style={styles.refresh}
+            onPress={this.updateWeather}>
+            <Text>
+              Check Weather
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+
+class CityWeather extends React.Component {
+  
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.cwItem}>
+        <Text style={styles.cwItemText}>{this.props.name}</Text>
+        <Text style={styles.cwItemText}>{this.props.temp}</Text>
+    </View>
+    );
+  }
+}
+
+export default class Step4 extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      cities: [
+        { name: "New York", temp: 80, key: 'ny' },
+        { name: "Chicago", temp: 74, key: 'chi' },
+        { name: "Los Angeles", temp: 72, key: 'la' },
+        { name: "Miami", temp: 93, key: 'mia' },
+        { name: "Anchorage", temp: 45, key: 'anc'}
+      ]
+    };
+  }
+
+  updateWeather = () => {
+    let updatedCities = [];
+    for (let c of this.state.cities) {
+      c.temp = Math.round(Math.random() * 100);
+      updatedCities.push(c);
+    }
+    this.setState({
+      cities: updatedCities
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Totally Accurate Weather</Text>
+        </View>
+        <View style={styles.body}>
+          <FlatList
+            data={this.state.cities}
+            renderItem={({item}) => 
+              <CityWeather name={item.name} temp={item.temp}/>
             }
             />
         </View>
